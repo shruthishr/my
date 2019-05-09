e0274eca299a745387b094cc0c4e998aff21c8aa
shruthiyasam <shruthiyasam83@gmail.com>
2019-05-09 20:54:20 +0530

    add

---
+++ my/src/my/add.java
@@ -0,0 +1,14 @@
+package my;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=7;
+		int b=9;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
